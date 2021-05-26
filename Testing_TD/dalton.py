f24038ef56957a92d7ff4bd97dae59b6076b4968
nisbetda <70228149+nisbetda@users.noreply.github.com>
2021-05-25 21:06:39 -0500

    Delete Testing_TD directory
    Creating space for the new and improved bot

--- Testing_TD/dalton.py
+++
@@ -1,5 +0,0 @@
-# the td ameritrade account number 
-# can be found in the personal information tab under client services
-ACCOUNT_NUMBER = '270145485'
-
-#ACCOUNT_NUMBER = '425833352'
--- Testing_TD/data_pipeline.py
+++
@@ -1,3 +0,0 @@
-# pip install td-ameritrade-python-api==0.2.6
-# pip install --upgrade td-ameritrade-python-api
-
